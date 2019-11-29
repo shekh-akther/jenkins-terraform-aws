31d4ca70590d7e4191e29086aa23ee9c7e3f7cce
Shekh Akther <makther08@gmail.com>
2019-11-29 23:35:02 +0100

    initial commit.

---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "count" {
+  default = 1
+}
+
+variable "key_name" {
+  description = "Private key name to use with instance"
+  default     = "terraform"
+}
+
+variable "instance_type" {
+  description = "AWS instance type"
+  default     = "t2.micro"
+}
+
+variable "ami" {
+  description = "Base AMI to launch the instances"
+
+  # Tomcat AMI
+  default = "ami-62c86115"
+}
